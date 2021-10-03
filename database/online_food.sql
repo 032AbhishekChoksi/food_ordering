5915cedd39c638f4daed0acb090927c09f65e567
032 - Abhishek Choksi <19bmiit032@gmail.com>
2021-10-03 14:54:35 +0530

    Upto Ten Video Completed With Database Updated

--- database/online_food.sql
+++ database/online_food.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 25, 2021 at 07:02 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 03, 2021 at 09:21 AM
+-- Server version: 8.0.21
+-- PHP Version: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,13 +27,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -48,25 +50,27 @@ INSERT INTO `admin` (`id`, `name`, `username`, `password`, `email`) VALUES
 -- Table structure for table `banner`
 --
 
-CREATE TABLE `banner` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `banner`;
+CREATE TABLE IF NOT EXISTS `banner` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `image` varchar(100) NOT NULL,
   `heading` varchar(500) NOT NULL,
   `sub_heading` varchar(500) NOT NULL,
   `link` varchar(100) NOT NULL,
   `link_txt` varchar(100) NOT NULL,
-  `order_number` int(11) NOT NULL,
+  `order_number` int NOT NULL,
   `added_on` datetime NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `banner`
 --
 
 INSERT INTO `banner` (`id`, `image`, `heading`, `sub_heading`, `link`, `link_txt`, `order_number`, `added_on`, `status`) VALUES
-(1, '437902525_slider-1.jpg', 'Drink & Heathy Food', 'Fresh Heathy and Organic.', 'shop.php', 'Order Now', 1, '2021-08-28 07:47:49', 1),
-(2, '124663692_slider-2.jpg', 'Drink & Heathy Food', 'Fresh Heathy and Organic.', 'shop.php', 'Order Now', 2, '2021-08-28 08:28:44', 1);
+(1, '437902525_slider-1.jpg', 'Drink & Heathy Food', 'Fresh Heathy and Organic.', 'shop', 'Order Now', 1, '2021-08-28 07:47:49', 1),
+(2, '124663692_slider-2.jpg', 'Drink & Heathy Food', 'Fresh Heathy and Organic.', 'shop', 'Order Now', 2, '2021-08-28 08:28:44', 1);
 
 -- --------------------------------------------------------
 
@@ -74,13 +78,15 @@ INSERT INTO `banner` (`id`, `image`, `heading`, `sub_heading`, `link`, `link_txt
 -- Table structure for table `category`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `category` varchar(50) NOT NULL,
-  `order_number` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `order_number` int NOT NULL,
+  `status` int NOT NULL,
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `category`
@@ -98,15 +104,17 @@ INSERT INTO `category` (`id`, `category`, `order_number`, `status`, `added_on`) 
 -- Table structure for table `contact_us`
 --
 
-CREATE TABLE `contact_us` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `contact_us`;
+CREATE TABLE IF NOT EXISTS `contact_us` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mobile` varchar(15) NOT NULL,
   `subject` varchar(200) NOT NULL,
   `message` text NOT NULL,
-  `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `contact_us`
@@ -122,16 +130,18 @@ INSERT INTO `contact_us` (`id`, `name`, `email`, `mobile`, `subject`, `message`,
 -- Table structure for table `coupon_code`
 --
 
-CREATE TABLE `coupon_code` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `coupon_code`;
+CREATE TABLE IF NOT EXISTS `coupon_code` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `coupon_code` varchar(20) NOT NULL,
   `coupon_type` enum('P','F') NOT NULL,
-  `coupon_value` int(11) NOT NULL,
-  `cart_min_value` int(11) NOT NULL,
+  `coupon_value` int NOT NULL,
+  `cart_min_value` int NOT NULL,
   `expired_on` date NOT NULL,
-  `status` int(11) NOT NULL,
-  `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int NOT NULL,
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -139,14 +149,16 @@ CREATE TABLE `coupon_code` (
 -- Table structure for table `delivery_boy`
 --
 
-CREATE TABLE `delivery_boy` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `delivery_boy`;
+CREATE TABLE IF NOT EXISTS `delivery_boy` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `mobile` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `status` int(11) NOT NULL,
-  `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int NOT NULL,
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `delivery_boy`
@@ -162,15 +174,17 @@ INSERT INTO `delivery_boy` (`id`, `name`, `mobile`, `password`, `status`, `added
 -- Table structure for table `dish`
 --
 
-CREATE TABLE `dish` (
-  `id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `dish`;
+CREATE TABLE IF NOT EXISTS `dish` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category_id` int NOT NULL,
   `dish` varchar(100) NOT NULL,
   `dish_detail` text NOT NULL,
   `image` varchar(100) NOT NULL,
-  `status` int(11) NOT NULL,
-  `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int NOT NULL,
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=74 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `dish`
@@ -179,7 +193,8 @@ CREATE TABLE `dish` (
 INSERT INTO `dish` (`id`, `category_id`, `dish`, `dish_detail`, `image`, `status`, `added_on`) VALUES
 (1, 4, 'Gulab Jamun', 'Gulab Jamun', '977945963_862169053_gulab-jamun.jpg', 1, '2021-08-11 10:43:59'),
 (2, 1, 'Raj Kachori', 'Raj Kachori', '495788285_325195312_raj-kachori.jpeg', 1, '2021-08-11 10:46:06'),
-(3, 2, 'Chow mein', 'Chow mein', '836724175_Chowmein.jpg', 1, '2021-08-11 10:47:26');
+(3, 2, 'Chow mein', 'Chow mein', '836724175_Chowmein.jpg', 1, '2021-08-11 10:47:26'),
+(73, 4, 'Ice Cream', 'Chocolate Scoop', '123403715_ice_cream.jpg', 1, '2021-09-28 11:08:35');
 
 -- --------------------------------------------------------
 
@@ -187,14 +202,16 @@ INSERT INTO `dish` (`id`, `category_id`, `dish`, `dish_detail`, `image`, `status
 -- Table structure for table `dish_details`
 --
 
-CREATE TABLE `dish_details` (
-  `id` int(11) NOT NULL,
-  `dish_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `dish_details`;
+CREATE TABLE IF NOT EXISTS `dish_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dish_id` int NOT NULL,
   `attribute` varchar(100) NOT NULL,
-  `price` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `price` int NOT NULL,
+  `status` int NOT NULL,
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `dish_details`
@@ -204,7 +221,8 @@ INSERT INTO `dish_details` (`id`, `dish_id`, `attribute`, `price`, `status`, `ad
 (1, 3, 'Full', 300, 1, '2021-08-11 10:43:59'),
 (2, 3, 'Half', 170, 1, '2020-06-19 10:49:45'),
 (6, 1, 'Per Piece', 40, 1, '2021-08-11 10:46:06'),
-(7, 2, 'Per Piece', 110, 1, '2021-08-11 10:47:26');
+(7, 2, 'Per Piece', 110, 1, '2021-08-11 10:47:26'),
+(8, 73, 'Per Piece', 40, 1, '2021-09-28 11:08:35');
 
 -- --------------------------------------------------------
 
@@ -212,12 +230,14 @@ INSERT INTO `dish_details` (`id`, `dish_id`, `attribute`, `price`, `status`, `ad
 -- Table structure for table `order_detail`
 --
 
-CREATE TABLE `order_detail` (
-  `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `dish_details_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE IF NOT EXISTS `order_detail` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `dish_details_id` int NOT NULL,
   `price` float NOT NULL,
-  `gst` float NOT NULL
+  `gst` float NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -226,19 +246,21 @@ CREATE TABLE `order_detail` (
 -- Table structure for table `order_master`
 --
 
-CREATE TABLE `order_master` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `order_master`;
+CREATE TABLE IF NOT EXISTS `order_master` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `mobile` varchar(50) NOT NULL,
   `address` text NOT NULL,
   `total_price` float NOT NULL,
   `gst` float NOT NULL,
-  `delivery_boy_id` int(11) NOT NULL,
-  `payment_status` int(11) NOT NULL,
-  `order_status` int(11) NOT NULL,
-  `added_on` datetime NOT NULL
+  `delivery_boy_id` int NOT NULL,
+  `payment_status` int NOT NULL,
+  `order_status` int NOT NULL,
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -247,9 +269,11 @@ CREATE TABLE `order_master` (
 -- Table structure for table `order_status`
 --
 
-CREATE TABLE `order_status` (
-  `id` int(11) NOT NULL,
-  `order_status` varchar(50) NOT NULL
+DROP TABLE IF EXISTS `order_status`;
+CREATE TABLE IF NOT EXISTS `order_status` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_status` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -258,176 +282,26 @@ CREATE TABLE `order_status` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `mobile` varchar(15) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `status` int(11) NOT NULL,
-  `email_verify` int(11) NOT NULL,
+  `status` int NOT NULL,
+  `email_verify` int NOT NULL,
   `rand_str` varchar(20) NOT NULL,
-  `added_on` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `added_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `email`, `mobile`, `password`, `status`, `email_verify`, `rand_str`, `added_on`) VALUES
-(1, 'Abhishek Choksi', '19bmiit032@gmail.com', '7874376579', '$2y$10$SZ1lbgywISn5p01PiaY7ueB7/5hWnMJDrFmEJ9MCuPfMNgi/3kLum', 1, 1, 'gxbhvdvtqsqcucx', '2021-09-24 02:57:05');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `banner`
---
-ALTER TABLE `banner`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `contact_us`
---
-ALTER TABLE `contact_us`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `coupon_code`
---
-ALTER TABLE `coupon_code`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `delivery_boy`
---
-ALTER TABLE `delivery_boy`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `dish`
---
-ALTER TABLE `dish`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `dish_details`
---
-ALTER TABLE `dish_details`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order_detail`
---
-ALTER TABLE `order_detail`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order_master`
---
-ALTER TABLE `order_master`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `order_status`
---
-ALTER TABLE `order_status`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `banner`
---
-ALTER TABLE `banner`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `contact_us`
---
-ALTER TABLE `contact_us`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `coupon_code`
---
-ALTER TABLE `coupon_code`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `delivery_boy`
---
-ALTER TABLE `delivery_boy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `dish`
---
-ALTER TABLE `dish`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
-
---
--- AUTO_INCREMENT for table `dish_details`
---
-ALTER TABLE `dish_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `order_detail`
---
-ALTER TABLE `order_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `order_master`
---
-ALTER TABLE `order_master`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `order_status`
---
-ALTER TABLE `order_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+(2, 'Abhishek Choksi', '19bmiit032@gmail.com', '7874376579', '$2y$10$jhrgzxQ0FnItLCk3Zpg4P.ESlC2ta/9w.2MZgJ3fvz0y5rrXt1rrS', 1, 1, 'oruvcbmuekjpwms', '2021-10-03 08:58:30');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
